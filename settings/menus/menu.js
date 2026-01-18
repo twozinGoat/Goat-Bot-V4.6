6bdcd2a3c1c21c513f20157e72b0a13a57034e4e
twozin <yrlanlok@gmail.com>
2026-01-17 22:47:17 -0300

    Refactor menu formatting and structure

--- settings/menus/menu.js
+++ settings/menus/menu.js
@@ -13,62 +13,61 @@ function generateMenu() {
     const { date, time } = getCurrentDateTime();
 
     return `
-🩸♰ ola seja bem vindo♰🩸
-╔═══⚡∘◦◦∘⚡═══╗
-       *${config.NomeDoBot}*
-╚═══⚡∘◦◦∘⚡═══╝
+ola bem vindo ao GoatBot
+┏━━━⌬━━━┓
+┃  *${config.NomeDoBot}*
+┗━━━⌬━━━┛
 
 🗓️ _${date}_
 🕰️ _${time}_
 👑 _Dono: ${config.NickDono}_
 
-┏━━━🔪⌬🔪━━━┓
-┃     *🪓  menu🪓*
-┣━━「 🕷 *PRINCIPAL* 」 
-┃  🔪 ${config.prefix}menuadm
-┃ 🕷 ${config.prefix}brincadeiras
-┃ 🪓 ${config.prefix}menulogos
+┏━━━⌬━━━━┓
+┃     *menu*
+┣━「 *PRINCIPAL* 」 
+┃  ${config.prefix}menuadm
+┃  ${config.prefix}brincadeiras
+┃  ${config.prefix}menulogos
 
-┣━━「 ⚙️ *SISTEMA* 」
-┃ 🪓  ${config.prefix}ping
-┃ 🔪 ${config.prefix}status
-┃ 🔪  ${config.prefix}stats
-┃ 🪓 ${config.prefix}roubar
-┃ 🕷 ${config.prefix}revelar
-┃ 🔪 ${config.prefix}sticker
-┃ 🪓 ${config.prefix}toimg
+┣━「 ⚙️ *SISTEMA* 」
+┃  ${config.prefix}ping
+┃  ${config.prefix}status
+┃  ${config.prefix}stats
+┃  ${config.prefix}roubar
+┃  ${config.prefix}revelar
+┃  ${config.prefix}sticker
+┃  ${config.prefix}toimg
 
-┣━━「 🎶 *CONVERSÃO MÁGICA* 」
-┃ 🪭 ${config.prefix}totext
-┃ ✝️ ${config.prefix}ptvmsg
-┃ 🔯 ${config.prefix}attp
-┃ ☯️ ${config.prefix}ttp
-┃ 🕎 ${config.prefix}gerarlink
-┃ ☮️ ${config.prefix}rvisu
+┣━「 🎶 *CONVERSÃO MÁGICA* 」
+┃  ${config.prefix}totext
+┃  ${config.prefix}ptvmsg
+┃  ${config.prefix}attp
+┃  ${config.prefix}ttp
+┃  ${config.prefix}gerarlink
+┃  ${config.prefix}rvisu
 
-┣━━「 📥 *DOWNLOADS FESTIVOS* 」
-┃ ☸️ ${config.prefix}tomp3
-┃ 🪓 ${config.prefix}shazam
-┃ 🔪 ${config.prefix}play
-┃ 🔪 ${config.prefix}play2
-┃ 🔪 ${config.prefix}play3
-┃ 🕷 ${config.prefix}sc
-┃ 🪓 ${config.prefix}ttk
-┃ 🔪 ${config.prefix}ttk2
-┃ 🕷 ${config.prefix}tiktok
-┃ 🔪 ${config.prefix}tiktok2
-┃ 🪓 ${config.prefix}kwai
-┃ 🕷 ${config.prefix}instamp4
-┃ 🪓 ${config.prefix}instamp3
-┃ 🔪 ${config.prefix}Pintemp4
-┃ 🩸 ${config.prefix}Pintemp3
-┃ 🪭 ${config.prefix}Pinterest
-┃ 🔪${config.prefix}Pinterest2
+┣━「 📥 *DOWNLOADS FESTIVOS* 」
+┃  ${config.prefix}tomp3
+┃  ${config.prefix}shazam
+┃  ${config.prefix}play
+┃  ${config.prefix}play2
+┃  ${config.prefix}play3
+┃  ${config.prefix}sc
+┃  ${config.prefix}ttk
+┃  ${config.prefix}ttk2
+┃  ${config.prefix}tiktok
+┃  ${config.prefix}tiktok2
+┃  ${config.prefix}kwai
+┃  ${config.prefix}instamp4
+┃  ${config.prefix}instamp3
+┃  ${config.prefix}Pintemp4
+┃  ${config.prefix}Pintemp3
+┃  ${config.prefix}Pinterest
+┃  ${config.prefix}Pinterest2
 
-┣━━「 👤 *PERFIL* 」
+┣━「 👤 *PERFIL* 」
 ┃  ${config.prefix}perfil
-
-┗━━━⚡⌬⚡━━━┛
+┗━━━━⌬━━━━┛
 
 `;
 }
